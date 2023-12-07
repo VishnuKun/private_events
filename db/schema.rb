2a2340cb6be2f5416b25447bd69032a56742c989
Vishnu <vishnukun954AV@protonmail.com>
2023-12-07 14:06:42 +0545

    Added username to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_07_081604) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_07_082111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_081604) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.datetime "locked_at"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
