d53e0aec34a3dc20b4824d45fc6897345df42c9b
Vishnu <vishnukun954AV@protonmail.com>
2023-12-08 18:01:35 +0545

    Added associations for the three models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,10 @@
 class Event < ApplicationRecord
+  # An event can have many attendees (users). The 'attendances' table is used as a 'through' table.
+  has_many :attendances, foreign_key: :attended_event_id
+  has_many :attendees, through: :attendances, source: :attendee
+
+  # An event belongs to a creator (a user who created the event)
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
-  has_and_belongs_to_many :users
 
   validates :creator, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :events
+  # The user can attend many events. The 'attendances' table is used as a 'through' table.
+  has_many :attendances, foreign_key: :attendee_id
+  has_many :attended_events, through: :attendances, source: :attended_event
+
+  # A user can create many events (these are not the events they have attended)
   has_many :created_events, class_name: 'Event', foreign_key: 'creator_id'
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable,:lockable, :trackable
-
 end
