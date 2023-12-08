ef3a8fae5385c703b97c556437ed1e705af56e37
Vishnu <vishnukun954AV@protonmail.com>
2023-12-08 13:08:15 +0545

    Added HMABT association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :events
+  has_many :created_events, class_name: 'Event', foreign_key: 'creator_id'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable,:lockable, :trackable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_08_062827) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_08_071407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_08_062827) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "events_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
   end
 
   create_table "homes", force: :cascade do |t|
